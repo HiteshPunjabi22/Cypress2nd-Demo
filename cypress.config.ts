779413d48dc65629a09c967b8c1b27b51d24a2e5
Hitesh Punjabi <Hiteshpunjabi@WinjitTechnologiesPvtLtd.local>
2022-12-14 14:08:43 +0530

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  projectId: '5fgbic',
+  reporterOptions: {
+    charts: true,
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
